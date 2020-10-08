28fc384e93f90dc4e1ec23c4118ce974448184cd
nimaeivazzadeh <nima.eivazzadehkaljahi@yahoo.com>
2020-10-08 15:50:34 +0200

    I made an second agent class with the name agent_2 to inherite frm class agent_1

--- ABM_STARTING_POINT/agents.py
+++ ABM_STARTING_POINT/agents.py
@@ -1,15 +1,27 @@
-class Agents():
+class agent_1():
     def method1(self):
         print("agents class design is under development")
     
     def method2(self, params):
-        print("my parameters" + params)
+        print("my parameters: " + params)
+
+class agent_2(agent_1):
+    def method1(self):
+        agent_1.method1(self)
+        print("call agent_1 methods inside agent_2 class")
+    
+    def method2(self, params):
+        print("parameters in agent_2: " + params)
 
 def main():
 
-    c = Agents()
+    c = agent_1()
     c.method1()
     c.method2("This is parametes variable")
+
+    c2 = agent_2()
+    c2.method1()
+    c2.method2("These are parameters in method 2 class agent_2")
 
 
 if __name__ == "__main__":
