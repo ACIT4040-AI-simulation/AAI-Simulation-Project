bc2cd841cbe8570bf539ea3a973817c7f8d4dac0
nimaeivazzadeh <nima.eivazzadehkaljahi@yahoo.com>
2020-10-08 15:23:58 +0200

    The new file defines agen class and its parameters

---
+++ ABM_STARTING_POINT/agents.py
@@ -0,0 +1,17 @@
+class Agents():
+    def method1(self):
+        print("agents class design is under development")
+    
+    def method2(self, params):
+        print("my parameters" + params)
+
+def main():
+
+    c = Agents()
+    c.method1()
+    c.method2("This is parametes variable")
+
+
+if __name__ == "__main__":
+    main()
+
