67457fa06c62880c99e5981398e8199863b58981
Kassahun G Degena <s341460@oslomet.no>
2020-10-27 04:12:08 +0100

    Agent class update

--- PyCX-master/abm-evo.py
+++ PyCX-master/abm-evo.py
@@ -5,15 +5,12 @@ from classRoom import classRoom
 import json
 import random
 
-
-
 import copy as cp
 p_init = 1000. #initial population
 
-#infected ppl start from one
 inf_rate = 0.10 # initial infaction rate in a population
 mask_rate = 0.4 # percentage of mask users
-dr = 1.0 # death rate of infected ppl
+dr = 0.01 # death rate of infected ppl
 rr = 0.1 # recovery rate
 
 f_init = 0.10 # initial fox population
@@ -34,15 +31,7 @@ def upload_agents_json(fileName):
         agentObjList.append(temp)
     return agentObjList
 
-
-def upload_classroom_json(fileName):
-    classRoomList = json.load(open(fileName))
-    classAgentsList = []
-
-    for classroom in classRoomList:
-        temp = classRoom(classroom['class_id'], classroom['class_type'], classroom['length'], classroom['width'], classroom['sitting_capacity'], classroom['cleaning_cycle'], classroom['clean_status'], classroom['antibac_dispenser'], classroom['ventilation_grade'])
-        classAgentsList.append(temp)
-    return classAgentsList    
+ 
 
 def initializeAgents():
     agentsList = upload_agents_json("agent1000.json")
@@ -58,58 +47,38 @@ def initializeAgents():
     for ag in infectedList:
         ag.status= 'I'
     
-#this loop will be taken out to classrooms acording to the sizes.
+    #this loop will be taken out to classrooms acording to the sizes.
     for ag in agentsList:
         ag.x = random.random()
         ag.y = random.random()
     return agentsList
 
-def initializeRooms():
-    classRoomList = upload_classroom_json('classrooms2.json')
-    return classRoomList
-        
-def alocateAgentsinclass(agentlist, roomlist):
-    x = 0
-    for ag in agentlist:
-        roomlist[x%15].agentsList.append(ag)
-        ag.whereAmI = roomlist[x%15]
-        x+=1
         
 def initialize():
     global agentsList,inf_data, classRoomList
 
     agentsList = initializeAgents()
-    classRoomList = initializeRooms()
-        
-    alocateAgentsinclass(agentsList, classRoomList)
-    
+   
 def observe():
     global agentsList, classRoomList
     
-    no_classes = len(classRoomList)
-    print('no classes = ', no_classes )
-    row = 5
-    col = 3
+    subplot(2, 1, 1)
+    cla()
+    infected = [ag for ag in agentsList if ag.status == 'I']
+    if len(infected) > 0:
+        x = [ag.x for ag in infected]
+        y = [ag.y for ag in infected]
+        plot(x, y, 'ro')
     
-    for i in range(15):  
-        subplot(row, col, i+1)
-        cla()
-        infected = [ag for ag in classRoomList[i].agentsList if ag.status == 'I']
-        if len(infected) > 0:
-            x = [ag.x for ag in infected]
-            y = [ag.y for ag in infected]
-            plot(x, y, 'ro')
+    suspected = [ag for ag in agentsList if ag.status == 'S']
+    if len(suspected) > 0:
+        x = [ag.x for ag in suspected]
+        y = [ag.y for ag in suspected]
+        plot(x, y, 'b.')
+    axis('image')
+    axis([0, 1, 0, 1])
+    title('Enviroment ')
         
-        suspected = [ag for ag in classRoomList[i].agentsList if ag.status == 'S']
-        if len(suspected) > 0:
-            x = [ag.x for ag in suspected]
-            y = [ag.y for ag in suspected]
-            plot(x, y, 'b.')
-        axis('image')
-        axis([0, 1, 0, 1])
-        title('classRoom ')
-        
-
 def update_one_agent():
     global agentsList
     if agentsList == []:
@@ -124,15 +93,8 @@ def update_one_agent():
     ag.x = 1 if ag.x > 1 else 0 if ag.x < 0 else ag.x
     ag.y = 1 if ag.y > 1 else 0 if ag.y < 0 else ag.y
 
-    # detecting collision and simulating death or birth
-# =============================================================================
-#     neighbors = [nb for nb in agents if nb.type != ag.type
-#                  and (ag.x - nb.x)**2 + (ag.y - nb.y)**2 < cdsq]
-# 
-# =============================================================================
-    classroom = ag.whereAmI
-    neighbors = [nb for nb in classroom.agentsList if (ag.x - nb.x)**2 + (ag.y - nb.y)**2 < cdsq]
-    ag.behavior(neighbors)
+    neighbors = [nb for nb in agentsList if (ag.x - nb.x)**2 + (ag.y - nb.y)**2 < cdsq]
+    
     
 def update():
     global agentsList, classRoomList
--- PyCX-master/agent.py
+++ PyCX-master/agent.py
@@ -8,6 +8,9 @@ Created on Mon Oct 12 21:30:26 2020
 
 import json
 import random
+from math import sqrt
+
+s_distance_standard = 0.01
 
 class agent():
     def __init__(self, id_no, age, sex, status, mask, antibac, socialDistance):
@@ -23,9 +26,6 @@ class agent():
         self.infect_to_List = []
         self.whereAmI = 0
         
-        # print("agents class design is under development")
-    
-    #testing and debug
     def tostring(self):
         print(self.id_no, 'lala')
         print(self.age)
@@ -38,64 +38,57 @@ class agent():
         
     #perceptionsList is a list of objects around (object, agent, .....
     # )
+    def distance(self ag):
+        return sqrt((self.x-agent_in.x)**2 + (self.y-agent_in.y)**2)
+    
     def behavior(self,perceptionsList):
-    #iterate perseption list
-        for perception in perceptionsList:
-            if isinstance(perception, agent):
-                self.perceptAgent(self,perception)
-            else:
-                pass
-            '''
-            agent_action = random(1:5)
-            	if agent_state[agent_action]:
-            		action = 
-            	elif perception == "door":
-            		action= "open_door"
-            	elif perception == "enter_ corridor":
-            		action = "move_to_class"
-            	elif perception == "class":
-            		action= "open_door"
-            	else:
-            		action = agent_state
-            	return (action)
-            '''
-    def perceptAgent(self, agent_in):
-        #agent_actions = [[1,"passing"], [2,"cuffing"],[3,"greeting_by_hand"], [4,"talking_to"],[5,"moving_class"]]
-        myaction = random(1,5)  
-        agent_action = random(1,5) 
-        if myaction == 1:
+# # =============================================================================
+# #     #agent actions[   [1,"passing"], 
+#                         [2,"cuffing"],
+#                         [3,"greeting_by_hand"], 
+# #                       [4,"talking_to_one"],
+#                         [5,"Talking with group"]]
+# =============================================================================
+        myaction = random.randint(1,5)
+        if myaction ==1:
             pass
         elif myaction == 2:
-            pass 
-            ''' if both caff, one cuff, 
-            lets have data about the probablity(data collection) and matrix from test teams
-            '''
-        elif myaction == 3:
-            # xOr =  (a and not b) or (not a and b)
-            '''assume hanshake is 70% transmission rate
-                if mask the risk will half
-                if sanitise  25% less
-                then we have to get probablity( from data collection) and matrix from test teams
-                change_status
-                '''
-                #calcprobablity(self, agent)
-            if (self.status == 'I' and agent_in.status != 'I'):
-                #calcprobablity(self, agent)
-                prob = 0.9
-                if prob > 0.75:
-                    agent_in.status =  'I'
-                
-                pass
-            elif(self.status != 'I' and agent_in.status == 'I'):
-                #if my probablitiy gigevs me infected
-                self.infected(agent)
+            for ag in perceptionsList:
+                will_get_infected(ag)
+        elif myaction == 3: 
+            ag = choice(perceptionsList) 
+            will_get_infected(ag)
 
-        elif myaction == 4: 
-            pass
+        elif myaction == 4:
+            ag = choice(perceptionsList) 
+            will_get_infected(ag)
+
         elif myaction == 5:
-            pass
-        elif myaction == 6:
-            pass
+            for ag in perceptionsList:
+                will_get_infected(ag)
+     
+        for perception in perceptionsList:
+            if isinstance(perception, agent):
+                self.perceptAgent(perception)
+            else:
+                pass
+            
+    def will_get_infected(self, agent_in):
+               
+            if (self.status == 'I' and agent_in.status != 'I'):
+                prob = calcprobablity(self, agent)
+                if  choice([True, False], 1, p=[prob, 1-prob]):
+                    agent_in.status =  'I'
+                    agent_in.infect_By = self
+                    self.infect_to_List.append(agent_in)
+                
+            elif(self.status != 'I' and agent_in.status == 'I'):
+                prob = calcprobablity(self, agent)
+                if  choice([True, False], 1, p=[prob, 1-prob]):
+                    self.status =  'I'
+                    self.infect_By = agent_in
+                    agent_in.infect_to_List.append(self)
+                    
                     
     def perceptObject(self, object):
         #to be coded after we implment classrooms and object classes.
@@ -106,10 +99,37 @@ class agent():
         self.infected_By = agent
         #agent.infect_to_List.append(self)
 
-'''
-We need to consider 2 interactions with: 1.Interaction between the agent and the other agent(s) 
-					 2.Interaction between the agent and the environment 
-'''   
+
+    def calcprobablity(self, agent_in)    :
+        #mask
+        tr = 0
+       
+        if (self.mask == True and agent_in.mask == True):
+            tr = 0.015
+        elif ((self.mask == True and agent_in.mask == False) or (self.mask == False and agent_in.mask == True):
+            tr = 0.05
+        else:
+            tr = 0.70
+        
+        #Sanitiser
+        if (self.antibac >3 and agent_in.antibac >3):
+            tr = tr*0.16
+        elif (self.antibac >=5 or agent_in.antibac >=5):
+            tr = tr*0.16  
+        
+# =============================================================================
+#         #distance
+#         distance = sqrt((self.x-agent_in.x)**2 + (self.y-agent_in.y)**2)
+# =============================================================================
+        if distance(self, agent_in) >= s_distance_standard:
+            tr = tr * 0.82
+        return tr
+# =============================================================================
+# To be Done
+# We need to consider 2 interactions with: 1.Interaction between the agent and the other agent(s) 
+# 					 2.Interaction between the agent and the environment 
+#   
+# =============================================================================
 #for test purposes
 def main():
 
@@ -118,8 +138,6 @@ def main():
     for agentObj in agenList:
         temp = agent(agentObj['id_no'], agentObj['age'], agentObj['gender'], agentObj['status'], agentObj['mask'], agentObj['antibact'], agentObj['socialDistance'])
         print(temp.tostring())
-        
-    
         
 
 if __name__ == "__main__":
